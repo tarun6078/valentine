1003502c95e82297c6073d66170a7af3075899ca
tarun607 <82271804+tarun607@users.noreply.github.com>
2022-01-16 18:34:33 +0530

    phi

---
+++ config.js
@@ -0,0 +1,14 @@
+// 霸都丶傲天 2019/7/9  https://Github.com/AJLoveChina
+var config = {
+    // 下面的句子不一定非要7句， 你也可以改成4句，2句话都可以
+    lines: [
+       "Happy Birthday Day",
+       "To the most Intelligent Boy",
+
+    ],
+    
+    date: "2021-07-20T20:15:00",
+    
+    names: ["Tarun", "my love"],
+    audio: "phir.mp3"
+}
