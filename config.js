86c2ffb4e89160f35cd4f98b77c539d26d91d38e
tarun6078 <88580541+tarun6078@users.noreply.github.com>
2022-01-20 13:09:31 +0530

    happdree

--- config.js
+++ config.js
@@ -2,13 +2,25 @@
 var config = {
     // 下面的句子不一定非要7句， 你也可以改成4句，2句话都可以
     lines: [
-       "Happy Birthday Day",
-       "To the most Intelligent Boy",
+       "You come in my dreams",
+       "You come in my thoughts",
+       "Your cute face and dreamy eyes",
+       "Save meltedmy heart in knots",
+       "Whether it is morning or night",
+       "I just can seem to get you",
+       "Out of my mind or sight",
+       "come, pacify my beating heart",
+       "Yes, I have fallen for you",
+       "Give me a nice warm hug"
+       "And say that you love me too..."
+                     "By- Tarun",
+  
+        
 
     ],
     
     date: "2021-07-20T20:15:00",
     
-    names: ["Tarun", "my love"],
+    names: ["HAPPY", "DAY"],
     audio: "phir.mp3"
 }
