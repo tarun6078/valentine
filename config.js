8a6c9088aabf9a396d053eaeb60dc1a23eab24f6
tarun6078 <88580541+tarun6078@users.noreply.github.com>
2022-02-02 10:10:24 +0530

    ddlgj

--- config.js
+++ config.js
@@ -2,22 +2,16 @@
 var config = {
     // 下面的句子不一定非要7句， 你也可以改成4句，2句话都可以
     lines: [
-       "You come in my dreams",
-       "You come in my thoughts",
-       "Your cute face and dreamy eyes",
-       "Save melted my heart in knots",
-       "Whether it is morning or night",
-       "I just can seem to get you",
-       "Out of my mind or sight",
-       "come, pacify my beating heart",
-       "Yes, I have fallen for you",
-       "Give me a nice warm hug", 
-       "And say that you love me too...", 
-                     "By- Tarun",
-  
+       "On this special day of Valentine's Day",
+       "I wish you long lasting joy",
+       "endless love",
+       "blessings to share",
+       "power to achieve your dreams",
+       "And wonderful moments to remember",
+       "all through your life!♥♥♥",
         
-
-    ],
+                     "By- Tarun",
+   ],
     
     date: "2021-07-20T20:15:00",
     
